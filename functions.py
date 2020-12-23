555c11f069af292da23d64ecdb521092c3786712
Yaroslav Berlinsky <58460556+berlinium1@users.noreply.github.com>
2020-12-23 16:56:33 +0200

    last changes

--- functions.py
+++ functions.py
@@ -1,6 +1,17 @@
-def creating_strings(list, message=' '):
-    list = (input(message)).split()
-    return list
+def creating_strings(list1, list2, sep):
+    word=""
+    for i in list1:
+        flag = 0
+        for j in sep:
+            if i==j:
+                 flag=1
+        if flag==0:
+            word+=i
+        else:
+            list2.append(word)
+            word=""
+    list2.append(word)
+    return list2
 def comparing_words(list1, list2):
     f=0; index_new=0; list3=[]
     for word1 in list1:
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from functions import creating_strings, comparing_words, final_output
-
+row1=input("Input string1: ")
+row2=input("Input string2: ")
+#тут лежат сеператоры в списке
+sep = [' ', ',', ', ', ';', ':', '-', '(', ')', '[', ']', '{', '}']
+############
 string1, string2, fn_string = [], [], []
-string1 = creating_strings(string1, "Input string#1: ")
-string2 = creating_strings(string2, "Input string#2: ")
+string1 = creating_strings(row1, string1, sep)
+string2 = creating_strings(row2, string2, sep)
 string3 = comparing_words(string1, string2)
 final_output(string3)
+
+#for tests
+#1: zero one two three four five six seven eight nine ten ten eleven кпі
+#2: zero two six six six five 098 кпі
+
+
+#1 help:kpi;python[jointed]{c++};;;hghg
+#2 help, hghg
